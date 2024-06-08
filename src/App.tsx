47ccb58b06b94f1f8d64dfbcf87a158a552a5997
Fernando Meneses <fmg1808@uc.cl>
2024-06-07 20:50:45 -0400

    feat: add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
+import React from 'react';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import Login from './views/Login';
 
 const App = () => {
     return (
-        <>
-            <Login />
-        </>
+        <Router>
+            <React.Fragment>
+            <Routes>
+                <Route 
+                path="/" 
+                element={<Login />}
+                />
+            </Routes>
+            </React.Fragment>
+        </Router>
+        // <>
+        //     <Login />
+        // </>
     );
     }
 
