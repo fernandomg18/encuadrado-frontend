a71150e2f7731d43cdea0a40c993461441f597e2
Fernando Meneses <fmg1808@uc.cl>
2024-06-08 17:10:21 -0400

    feat: add shadcn ui

--- src/views/Login.tsx
+++ src/views/Login.tsx
@@ -1,24 +1,56 @@
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
+
+const userData = {
+    username: "admin",
+    password: "admin"
+}
 
 const Login = () => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleLogin = () => {
+        if (username === userData.username && password === userData.password) {
+            console.log("Login successful");
+        } else {
+            console.log("Login failed");
+        }
+    }
+
+
     return (
-        <div className="flex flex-row justify-center items-center h-screen">
-            <div className="w-1/2">
-                <h1 className="ml-16 font-bold text-5xl text-gray-600">
+        <div className="-mt-10 flex flex-col justify-center items-center h-screen">
+                <h1 className="mb-10 font-bold text-5xl text-gray-600">
                     🔶 encuadrado
                 </h1>
-            </div>
-            <div className="w-1/2 flex flex-col items-center">
-                <div className="w-3/6 bg-gradient-to-br from-orange-500 to-orange-500 p-10 rounded-xl shadow-2xl text-white">
+                <div className="w-1/4 bg-gradient-to-br from-orange-500 to-orange-500 p-10 rounded-xl shadow-2xl text-white">
                     <h4 className="text-xl mb-1">User</h4>
-                    <input className="text-gray-700 rounded-md p-1 mb-5 w-full" type="text" placeholder="Username" />
+                    <input 
+                      className="text-gray-700 rounded-md p-1 mb-5 w-full" 
+                      type="text" 
+                      placeholder="Username" 
+                      value={username}
+                      onChange={(e) => setUsername(e.target.value)}
+                    />
                     <h4 className="text-xl mb-1">Password</h4>
-                    <input className="text-gray-700 rounded-md p-1 w-full" type="password" placeholder="Password" />
+                    <input 
+                      className="text-gray-700 rounded-md p-1 w-full" 
+                      type="password" 
+                      placeholder="Password" 
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                    />
                     
-                    <button className="bg-white text-gray-700 hover:bg-gray-200 hover:text-gray-700 active:bg-gray-600 active:text-white p-2 rounded-md mt-7 flex justify-center w-full text-xl">
+                    <Button 
+                      className="w-full mt-5"
+                      variant="secondary" 
+                      onClick={handleLogin}
+                    >
                         Login
-                    </button>   
+                    </Button>
                 </div>
-            </div>
+                
         </div>
     );
     }
